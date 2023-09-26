13b5bc134ab299aa024e6cec9b03be5ce42b698c
bayushidzen <zhek_wh@mail.ru>
2023-09-26 22:14:25 +0300

    Добавьте файлы проекта.

---
+++ AppSecondBlock/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSecondBlock
+{
+    internal class Animal
+    {
+        private String _name;
+        private String _species;
+        int _age;
+
+        public Animal(String name, String species, int age)
+        {
+            this._name = name;
+            this._species = species;
+            this._age = age;
+        }
+        public override string ToString()
+        {
+            return $"Кличка: {_name}, Животное: {_species}, Возраст: {_age}".ToString();
+        }
+    }
+
+}
---
+++ AppSecondBlock/Program.cs
@@ -0,0 +1,114 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+
+namespace AppSecondBlock;
+
+class AnimalNote
+{
+    public static void Main()
+    {
+        IntroText();
+        Console.WriteLine("Выберите пункт меню");
+        int input = Convert.ToInt16(Console.Read());
+        
+        Console.WriteLine(input);
+        Selector(input);
+    }
+
+    private static void IntroText()
+    {
+        Console.WriteLine("Меню:");
+        Console.WriteLine("1) Просмотреть записи о всех животных  2) Добавить новое животное");
+        Console.WriteLine("3) Перевести животное в другой заповедник 4) Скоро будут новые функции");
+    }
+    private static void Restart()
+    {
+        Console.WriteLine("Хотите продолжить работу? \n");
+        Console.WriteLine("1) Да 2) Нет");
+        int input = Convert.ToInt16(Console.ReadKey());
+        switch (input)
+        {
+            case '1':
+                Main();
+                break;
+            case '2':
+                Console.WriteLine("Всего доброго! Благодарим за использование нашего приложения");
+                break;
+            default:
+                Console.WriteLine("Выберите, пожалуйста, пункт из меню");
+                Restart();
+                break;
+        }
+    }
+    private static void Selector(int input)
+    {
+        switch (input)
+        {
+            case '1':
+                ShowAnimals();
+                Restart();
+                break;
+            case '2':
+                AddAnimal();
+                Restart();
+                break;
+            case '3':
+                RemoveAnimal();
+                Restart();
+                break;
+            case '4':
+                Console.WriteLine("Следите за обновлениями на нашем сайте GB.ru");
+                Restart();
+                break;
+            default:
+                Console.WriteLine("Выберите, пожалуйста, пункт из меню");
+                Restart();
+                break;
+
+        }
+    }
+
+    private static void RemoveAnimal()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void AddAnimal()
+    {
+        Console.WriteLine("Меню:");
+        Console.WriteLine("Кого вы хотите добавить?");
+        Console.WriteLine("1) Домашние питомцы: коты, собаки, хомячки");
+        Console.WriteLine("2) Вьючные животные: лошади, верблюды, ослики");
+        int input = Convert.ToInt16(Console.ReadKey());
+        switch (input) 
+        {
+            case '1':
+                AddPet();
+                Restart();
+                break;
+            case '2':
+                AddPackAnimal();
+                Restart();
+                break;
+            default:
+                Console.WriteLine("Выберите, пожалуйста, пункт из меню");
+                Restart();
+                break;
+            }
+    }
+
+    private static void AddPackAnimal()
+    {
+        Console.WriteLine("Вьючные животные");
+    }
+
+    private static void AddPet()
+    {
+        Console.WriteLine("Домашние питомцы");
+    }
+
+    private static void ShowAnimals()
+    {
+        Console.WriteLine("Шоу животных");
+    }
+}
